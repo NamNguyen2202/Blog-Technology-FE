2406966252786f8eefdce2ff790df95aeb92e9ff
huyhuynh1711 <huyhuynhzz256@gmail.com>
2024-05-02 17:21:37 +0700

    sign-up

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
+// import { SignupComponent } from '../sign-up/sign-up.component';
 
 @Component({
   selector: 'app-home',
@@ -8,8 +9,12 @@ import { Router } from '@angular/router';
 })
 export class HomeComponent {
   constructor(private router: Router) {}
-  isCollapsed = false;
-  onClick() {
+  // isCollapsed = false;
+  onSignUp() {
+    this.router.navigateByUrl('sign-up'); // Replace with actual sign-in route path
+    // this.signUpComponent.show();
+  }
+  onSignIn() {
     this.router.navigateByUrl('sign-in'); // Replace with actual sign-in route path
   }
 }
