12bfa66bed0f0d83a0a40046b1d816d35f16c0b9
huyhuynh1711 <huyhuynhzz256@gmail.com>
2024-05-03 16:01:20 +0700

    chinh sua trang home

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
@@ -6,10 +6,14 @@ import { Router } from '@angular/router';
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
   constructor(private router: Router) {}
-  isCollapsed = false;
-  onClick() {
+  ngOnInit() {}
+  // isCollapsed = false;
+  onSignUp() {
+    this.router.navigateByUrl('sign-up');
+  }
+  onSignIn() {
     this.router.navigateByUrl('sign-in'); // Replace with actual sign-in route path
   }
 }
