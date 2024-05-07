58c28e4c0053f1f1e07ad96f365e8dfdf2624804
Nam Nguyễn <namnamm220201@gmail.com>
2024-05-07 16:03:37 +0700

    sua sign in 7/5

--- src/app/pages/sign-in/sign-in.component.ts
+++ src/app/pages/sign-in/sign-in.component.ts
@@ -1,14 +1,21 @@
-import { Component, OnInit } from '@angular/core';
-import { FormGroup, Validators,  FormControl, AbstractControl, ValidationErrors, FormBuilder } from '@angular/forms';
-import { ApiService } from './sign-in.service';
-import { Observable, Subject, map } from 'rxjs';
-import { PASSWORD_PATTERN, USERNAME_PATTERN } from './sign-in.data';
-import { Router } from '@angular/router';
+import { Component, OnInit } from "@angular/core";
+import {
+  FormGroup,
+  Validators,
+  FormControl,
+  AbstractControl,
+  ValidationErrors,
+  FormBuilder,
+} from "@angular/forms";
+import { ApiService } from "./sign-in.service";
+import { Observable, Subject, map } from "rxjs";
+import { PASSWORD_PATTERN, USERNAME_PATTERN } from "./sign-in.data";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-sign-in',
-  templateUrl: './sign-in.component.html',
-  styleUrls: ['./sign-in.component.css'],
+  selector: "app-sign-in",
+  templateUrl: "./sign-in.component.html",
+  styleUrls: ["./sign-in.component.css"],
 })
 export class SignInComponent implements OnInit {
   constructor(
@@ -16,73 +23,55 @@ export class SignInComponent implements OnInit {
     private api: ApiService,
     private Router: Router
   ) {}
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   formSubmit$ = new Subject<boolean | null>();
 
   validateUserNameFromApiDebounce() {
     return (control: AbstractControl): Observable<ValidationErrors | null> => {
       return this.api.validateUsername(control.value).pipe(
-            map((isValid) => {
-              if (!isValid) {
-                return null;
-              }
-              return {
-                usernameDuplicated: true,
-              };
-            })
-          );      
+        map((isValid) => {
+          if (!isValid) {
+            return null;
+          }
+          return {
+            usernameDuplicated: true,
+          };
+        })
+      );
     };
   }
 
-  formSignin  = new FormGroup({
-        username: new FormControl(
-          '',
-          [
-            Validators.required,
-            Validators.minLength(6),
-            Validators.maxLength(32),
-            Validators.pattern(USERNAME_PATTERN),
-          ],      
-         ),
-  
-        password: new FormControl('', [
-          Validators.required,
-          Validators.minLength(6),
-          Validators.maxLength(32),
-          Validators.pattern(PASSWORD_PATTERN),
-        ]),
-      },
+  formSignin = new FormGroup({
+    username: new FormControl("", [
+      Validators.required,
+      Validators.minLength(6),
+      Validators.maxLength(32),
+      Validators.pattern(USERNAME_PATTERN),
+    ]),
 
-    );
-  
+    password: new FormControl("", [
+      Validators.required,
+      Validators.minLength(6),
+      Validators.maxLength(32),
+      Validators.pattern(PASSWORD_PATTERN),
+    ]),
+  });
 
-    onSignin() {
-      this.validateUserNameFromApiDebounce()(this.formSignin.get('username')!).subscribe(validationResult => {
-        if (validationResult) {
-          this.formSignin.get('username')!.setErrors(validationResult);
-        } else {
-          this.Router.navigateByUrl('');
-        }
-      });
-    }
-    
- 
-
-
-
-  // onSignin(){
-  //   // console.log(this.formSignin.value) 
-  //   // this.Router.navigateByUrl('sign-up');
-    
-  // }
-
+  onSignin() {
+    this.validateUserNameFromApiDebounce()(
+      this.formSignin.get("username")!
+    ).subscribe((validationResult) => {
+      if (validationResult) {
+        this.formSignin.get("username")!.setErrors(validationResult);
+      } else {
+        this.Router.navigateByUrl("");
+      }
+    });
+  }
 
   hidePassword: boolean = true;
   togglePasswordVisibility(): void {
     this.hidePassword = !this.hidePassword;
   }
-
-  
 }
